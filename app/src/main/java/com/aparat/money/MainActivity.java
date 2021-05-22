76d370498cc3769b46401ff32499e338ef4ba174
Dima <dima.holubenko@gmail.com>
2021-05-22 10:25:47 +0300

    Initial commit

---
+++ app/src/main/java/com/aparat/money/MainActivity.java
@@ -0,0 +1,28 @@
+package com.aparat.money;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.loginBTN);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
